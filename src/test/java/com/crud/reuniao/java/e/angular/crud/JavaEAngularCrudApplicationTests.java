e432a3496e4670f3ee837f69028bbf30e42d6053
matheusosp <matheusosp@users.noreply.github.com>
2021-01-01 00:43:27 -0300

    first commit

---
+++ src/test/java/com/crud/reuniao/java/e/angular/crud/JavaEAngularCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.reuniao.java.e.angular.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEAngularCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
